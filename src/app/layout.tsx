09781918e5808f69682f672a8855265be5ca79b9
Katie Hom <schaka01@gmail.com>
2025-03-07 15:53:45 -0700

    feat: Update file source import (DSD-25)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Sofia_Sans } from 'next/font/google';
 import './globals.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Footer from './components/Footer';
 
 const sofiaSans = Sofia_Sans({
