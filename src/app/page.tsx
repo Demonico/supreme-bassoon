801df63ec6d27652e47787be1aba31684f41c5b0
Katie Hom <schaka01@gmail.com>
2025-03-07 12:19:09 -0700

    Import home page component into root page (DSD-25 )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HomePageComponent from '@/features/home/components/HomePageComponent';
+
 export default function Page() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <HomePageComponent />
+    </>
+  );
 }
