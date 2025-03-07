dabf93c92e83a530db4c903ab2996a6e77d22953
Katie Hom <schaka01@gmail.com>
2025-03-07 12:21:24 -0700

    feat: Create get started section (DSD-25)

---
+++ src/features/home/components/GetStartedSection.tsx
@@ -0,0 +1,17 @@
+import Button from '@/app/components/buttons/Button';
+
+export default function GetStartedSection() {
+  return (
+    <div className='flex flex-col items-center px-20 py-12'>
+      <p className='text-center pb-10 text-xl lg:w-3/4'>
+        What are you waiting for? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Vel similique expedita nisi, sequi minus, voluptate
+        aliquid iste consequatur, non iusto dolorum eveniet dolor cumque
+        ducimus! Necessitatibus eius autem adipisci harum.
+      </p>
+      <Button size='lg' fontWeight='semibold'>
+        Get started
+      </Button>
+    </div>
+  );
+}
