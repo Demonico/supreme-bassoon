2c84de40cf7d57d7b22df7daf757dfb43f78751c
Katie Hom <schaka01@gmail.com>
2025-03-07 15:52:46 -0700

    feat: Adjust padding on smaller screens (DSD-25)

--- src/features/home/components/GetStartedSection.tsx
+++ src/features/home/components/GetStartedSection.tsx
@@ -2,7 +2,7 @@ import Button from '@/app/components/buttons/Button';
 
 export default function GetStartedSection() {
   return (
-    <div className='flex flex-col items-center px-20 py-12'>
+    <div className='flex flex-col items-center px-10 lg:px-20 py-12'>
       <p className='text-center pb-10 text-xl lg:w-3/4'>
         What are you waiting for? Lorem, ipsum dolor sit amet consectetur
         adipisicing elit. Vel similique expedita nisi, sequi minus, voluptate
