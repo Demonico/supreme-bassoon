1ae32ceb02ff673e0e9cd53f192fc51a25439999
Katie Hom <schaka01@gmail.com>
2025-03-07 23:23:47 -0700

    feat: Create admin dashboard page (DSD-38)

---
+++ src/app/dashboard/admin/page.tsx
@@ -0,0 +1,5 @@
+import AdminDashboardPageComponent from '@/features/dashboard/components/admin-dashboard-page-component';
+
+export default function AdminDashboardPage() {
+  return <AdminDashboardPageComponent />;
+}
