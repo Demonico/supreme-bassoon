b7b5f45461e8e4d7a95fd3f46b4227311d6a21b1
Katie Hom <schaka01@gmail.com>
2025-03-07 23:24:01 -0700

    feat: Create admin dashboard page component

---
+++ src/features/dashboard/components/admin-dashboard-page-component.tsx
@@ -0,0 +1,10 @@
+import PartsInventoryTable from './parts-inventory-table';
+
+export default function AdminDashboardPageComponent() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <PartsInventoryTable />
+    </div>
+  );
+}
